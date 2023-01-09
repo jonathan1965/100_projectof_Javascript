db7464d28265ade9e24a5a0916f6fbbd7f967b54
jonathan1965 <jonakarisa@gmail.com>
2023-01-10 00:12:58 +0200

    make me better

--- public/CounterProject/counter.js
+++ public/CounterProject/counter.js
@@ -1,5 +1,18 @@
 const count = document.querySelector(".count");
+const add = document.querySelector('.add')
+const sub = document.querySelector('.sub')
+const resetCounter = document.querySelector('.resetCounter')
+let resetValue= "0"
 
-count.addEventListener("click", () => {
+add.addEventListener("click", () => {
     count.innerHTML ++;
+});
+
+resetCounter.addEventListener("click", () => {
+    count.innerHTML = resetValue;
 })
+
+sub.addEventListener("click", () => {
+    count.innerHTML --;
+});
+
