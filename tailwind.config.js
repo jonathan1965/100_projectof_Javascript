5acaa2e5a7330f8ebbc89ff4e3893a383ebe761f
jonathan1965 <jonakarisa@gmail.com>
2023-02-18 03:22:01 +0200

    make  me better

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
-
-
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors")
 module.exports = {
   content: [
-    './public/*/*.html/'
+    './public/*/*.html/',
+    './public/**/*.html/'
   ],
   theme: {
     extend: {
@@ -14,6 +14,21 @@ module.exports = {
         '150': '100rem',
         '200': '200rem',
         '300': '300rem'
+      },
+      colors: {
+        'cyan':colors.cyan,
+        'yellow':colors.gray,
+        'Slate':colors.Slate,
+        'Zinc':colors.Zinc,
+        'Neutral':colors.Neutral,
+        'Stone':colors.Stone,
+        'Red':colors.Red,
+        'Green':colors.Green,
+        'Blue':colors.Blue,
+        'Purple':colors.Purple,
+        'Rose':colors.Rose,
+        gray:colors.gray,
+
       },
     fontFamily: {
       Montserrat: ['Montserrat', 'sans-serif'],
